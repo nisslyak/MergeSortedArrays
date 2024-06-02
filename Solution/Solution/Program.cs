d5c32ecb53c3253612635d4244ec5c32415828f7
nisslyak <nissopodolyak@gmail.com>
2024-06-02 05:50:22 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public static int[] Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+
+        var index = 0;
+
+        if (nums2.Count() !=0)
+        {
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (nums1[i] == 0 && i >= m)
+                {
+                    nums1[i] = nums2[index];
+                    index++;
+                }
+            }
+        }
+
+
+        Array.Sort(nums1);
+
+        return nums1;
+    }
+
+    public static void Main(string[] args)
+    {
+        int m = Convert.ToInt32(Console.ReadLine());
+        var nums1 = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+        int n = Convert.ToInt32(Console.ReadLine());
+        var nums2 = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+        var mergedArray = Merge(nums1, m, nums2, n);
+        
+        Console.WriteLine(string.Join(" ", mergedArray));
+    }
+}
+
